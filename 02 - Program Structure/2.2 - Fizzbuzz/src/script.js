2c8a64e5603da12dc200eca8499a5cffe93fa9f9
jcasoya <jcasoya@gmail.com>
2023-05-13 17:49:17 +0200

    2.2 fizz, buzz & fuzzbuzz working

--- 02 - Program Structure/2.2 - Fizzbuzz/src/script.js
+++ 02 - Program Structure/2.2 - Fizzbuzz/src/script.js
@@ -2,13 +2,15 @@
 
 //Part One: log 1 to 100, if % by 3 log Fizz, if % by 5 log BUZZ
 for (let current = 1; current <= 100; current++) {
-    console.log(current)
-    if (current % 3 === 0) {
-      console.log("FIZZ");
-      continue;
-    }
-    if (current % 5 === 0) {
-      console.log("BUZZ");
-      continue;
-    }
+  number = console.log(current)
+  if (current % 3 === 0) {
+    let fizz = console.log("FIZZ");
   }
+  if (current % 5 === 0) {
+    let buzz = console.log("BUZZ");
+  }
+  if (current % 3 === 0 && current % 5 === 0) {
+    console.log("FIZZBUZZ");
+  }
+}
+
