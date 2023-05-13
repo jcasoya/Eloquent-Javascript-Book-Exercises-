c3b3e4850d78be0c2be450b04063d38295f075eb
jcasoya <jcasoya@gmail.com>
2023-05-13 18:01:17 +0200

    plus book solution

--- 02 - Program Structure/2.2 - Fizzbuzz/src/script.js
+++ 02 - Program Structure/2.2 - Fizzbuzz/src/script.js
@@ -1,16 +1,22 @@
 //2.1 FIZZBUZZ
 
 //Part One: log 1 to 100, if % by 3 log Fizz, if % by 5 log BUZZ
-for (let current = 1; current <= 100; current++) {
+for (let current = 1; current < 100; current++) {
   number = console.log(current)
   if (current % 3 === 0) {
-    let fizz = console.log("FIZZ");
+    console.log("FIZZ");
   }
   if (current % 5 === 0) {
-    let buzz = console.log("BUZZ");
+    console.log("BUZZ");
   }
   if (current % 3 === 0 && current % 5 === 0) {
     console.log("FIZZBUZZ");
   }
 }
 
+for (let n = 1; n <= 100; n++) {
+  let output = "";
+  if (n % 3 == 0) output += "Fizz";
+  if (n % 5 == 0) output += "Buzz";
+  console.log(output || n);
+}
