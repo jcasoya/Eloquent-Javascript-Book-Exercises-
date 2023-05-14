3bbd3e966554a8e919fbc46764898cabb23ebef0
jcasoya <jcasoya@gmail.com>
2023-05-14 15:44:29 +0200

    2.3 Chessboard my solution + Book's solution

--- 02 - Program Structure/2.3 - Chessboard/src/script.js
+++ 02 - Program Structure/2.3 - Chessboard/src/script.js
@@ -45,3 +45,19 @@ function chessboard(width, height) {
 
 console.log(chessboard(3, 6));
 
+let size = 8;
+
+let board = "";
+
+for (let y = 0; y < size; y++) {
+  for (let x = 0; x < size; x++) {
+    if ((x + y) % 2 == 0) {
+      board += " ";
+    } else {
+      board += "#";
+    }
+  }
+  board += "\n";
+}
+
+console.log(board);
