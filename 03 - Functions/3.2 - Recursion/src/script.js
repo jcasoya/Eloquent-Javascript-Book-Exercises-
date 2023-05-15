02f619001967fbb552b81d18854065ee367ba3bf
jcasoya <jcasoya@gmail.com>
2023-05-15 14:15:05 +0200

    3.2 my new solution working + book solution

--- 03 - Functions/3.2 - Recursion/src/script.js
+++ 03 - Functions/3.2 - Recursion/src/script.js
@@ -11,16 +11,27 @@ The function should accept a single parameter (a positive, whole number) and ret
 Test it on 50 and 75. See how it behaves on -1. Why? Can you think of a way to fix this?
 */
 
-// My Code
-const isEven = (number) => (number % 2  == 0) ? :
+// My Code = WRONG, not using a recursive function!!!
+// const isEven = (number) => (number % 2  == 0) ? true : false;
 
+// REDO wihtout looking at the solution again
+const isEven = function(n) {
+    if (n == 0) return true;
+    else if (n == 1) return false;
+    else if (n < 0) {
+        return isEven(-n);
+    } else {
+        return isEven(n - 2);
+    }
+    }
+    
 // Book solution
 
 // Tests
-console.log(isEven(2));
+console.log(isEven(50));
 // → true
 console.log(isEven(75));
 // → false
-console.log(isEven(-13));
+console.log(isEven(-1));
 // → ??
 
