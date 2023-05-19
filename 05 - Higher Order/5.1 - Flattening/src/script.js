434e4b5b847264a4fe7d458ab8d0be74a36ad246
jcasoya <jcasoya@gmail.com>
2023-05-19 12:21:32 +0200

    5.1 my solution

---
+++ 05 - Higher Order/5.1 - Flattening/src/script.js
@@ -0,0 +1,18 @@
+// Flattening
+
+/* 
+Use the reduce method in combination with the concat method to “flatten” an array of arrays 
+into a single array that has all the elements of the original arrays.
+*/
+
+let arrays = [[1, 2, 3], [4, 5], [6]];
+
+// Your code here.
+let flattenedArray = arrays.reduce((a, b) => {
+  return a.concat(b);
+}, []);
+
+console.log(flattenedArray);
+
+
+// → [1, 2, 3, 4, 5, 6]
